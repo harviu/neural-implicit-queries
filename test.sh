ac4bb741428ad922da7f9567c82c4f61e2e160ab
Haoyu Li <aide0740@gmail.com>
2023-02-28 10:20:16 -0500

    change gitlgnore

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=10:00:00
+#SBATCH --job-name=test
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus-per-node=1
+#SBATCH --account=PAS0027
+#SBATCH --output=osc_outputs/test.%j
+
+ml miniconda3
+ml cuda/11.6.2
+source activate implicit-env
+
+python src/main.py 
